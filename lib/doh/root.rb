42d07ce65a37d7e884f8d79a979b2b79b572cead
Makani Mason <gitauthor@pqmland.com>
2012-07-16 09:17:37 -0600

    switch from :: to . for all method calls

--- lib/doh/root.rb
+++ lib/doh/root.rb
@@ -14,29 +14,29 @@ module Doh
   end
 
   def self.find_root(start_directory, filename = 'dohroot', max_tries = 20)
-    rootfile = Doh::findup(start_directory, filename, max_tries)
+    rootfile = Doh.findup(start_directory, filename, max_tries)
     if rootfile
-      Doh::root = File.dirname(rootfile)
+      Doh.root = File.dirname(rootfile)
     end
   end
 
   def self.find_root_from_file(filepath = nil)
-    Doh::find_root(File.dirname(filepath || caller[0]))
+    Doh.find_root(File.dirname(filepath || caller[0]))
   end
 
   def self.find_root_from_path(path)
     if File.directory?(path)
-      Doh::find_root(path)
+      Doh.find_root(path)
     else
-      Doh::find_root(File.dirname(path))
+      Doh.find_root(File.dirname(path))
     end
   end
 
   def self.find_root_from_prog
-    Doh::find_root(File.dirname($PROGRAM_NAME))
+    Doh.find_root(File.dirname($PROGRAM_NAME))
   end
 
   def self.find_root_from_pwd
-    Doh::find_root(Dir.pwd)
+    Doh.find_root(Dir.pwd)
   end
 end
