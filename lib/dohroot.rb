daf7d160a98757b32358cb499cedf8689b0b8903
Makani Mason <gitauthor@pqmland.com>
2013-05-22 07:12:09 -0600

    formatting cleanup

--- lib/dohroot.rb
+++ lib/dohroot.rb
@@ -1,42 +1,46 @@
 require 'dohroot/findup'
 
 module Doh
-  class DohRootNotFoundException < Exception
-  end
-  def self.root
-    @root
-  end
+extend self
 
-  def self.root=(directory)
-    @root = directory
-    libdir = File.join(@root, 'lib')
-    $LOAD_PATH.push(libdir) if libdir
-  end
+class DohRootNotFoundException < Exception
+end
 
-  def self.find_root(start_directory, filename = 'dohroot', max_tries = 20)
-    rootfile = Doh.findup(start_directory, filename, max_tries)
-    if rootfile
-      Doh.root = File.dirname(rootfile)
-    end
-  end
+def root
+  @root
+end
 
-  def self.find_root_from_file(filepath = nil)
-    Doh.find_root(File.dirname(filepath || caller[0]))
-  end
+def root=(directory)
+  @root = directory
+  libdir = File.join(@root, 'lib')
+  $LOAD_PATH.push(libdir) if libdir
+end
 
-  def self.find_root_from_path(path)
-    if File.directory?(path)
-      Doh.find_root(path)
-    else
-      Doh.find_root(File.dirname(path))
-    end
-  end
-
-  def self.find_root_from_prog
-    Doh.find_root(File.dirname($PROGRAM_NAME))
-  end
-
-  def self.find_root_from_pwd
-    Doh.find_root(Dir.pwd)
+def find_root(start_directory, filename = 'dohroot', max_tries = 20)
+  rootfile = Doh.findup(start_directory, filename, max_tries)
+  if rootfile
+    Doh.root = File.dirname(rootfile)
   end
 end
+
+def find_root_from_file(filepath = nil)
+  Doh.find_root(File.dirname(filepath || caller[0]))
+end
+
+def find_root_from_path(path)
+  if File.directory?(path)
+    Doh.find_root(path)
+  else
+    Doh.find_root(File.dirname(path))
+  end
+end
+
+def find_root_from_prog
+  Doh.find_root(File.dirname($PROGRAM_NAME))
+end
+
+def find_root_from_pwd
+  Doh.find_root(Dir.pwd)
+end
+
+end # module Doh
--- lib/dohroot/findup.rb
+++ lib/dohroot/findup.rb
@@ -1,12 +1,15 @@
 module Doh
-  def self.findup(start_directory, filename, max_tries = 20)
-    curr_directory = start_directory
-    max_tries.times do
-      path = File.expand_path(File.join(curr_directory, filename))
-      return path if File.exist?(path)
-      return nil if (path == '/')
-      curr_directory = File.join(curr_directory, '..')
-    end
-    nil
+extend self
+
+def findup(start_directory, filename, max_tries = 20)
+  curr_directory = start_directory
+  max_tries.times do
+    path = File.expand_path(File.join(curr_directory, filename))
+    return path if File.exist?(path)
+    return nil if (path == '/')
+    curr_directory = File.join(curr_directory, '..')
   end
+  nil
 end
+
+end # module Doh
